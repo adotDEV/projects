2ecfa02aa7dceba60a5cfced84ab4e3b890aece5
adotau <75143918+adotDEV@users.noreply.github.com>
2020-12-24 12:51:57 -0300

    Update deepnude-install-script.sh

--- deepnude-install-script.sh
+++ deepnude-install-script.sh
@@ -6,10 +6,7 @@ echo "Would you like to install Python's dependencies and Pip?? [Y/n]"
 read insPython
 clear
 
-if [ $insPython == y ] || [ $insPython == Y ];
-
-then 
-
+if [ $insPython == y ] || [ $insPython == Y ]; then 
     mkdir tempPy
     cd tempPy
     
@@ -22,16 +19,16 @@ then
     ./configure --with-ensurepip=install
 
     sudo make -j 2 && sudo make altinstall
+    python3.9 -m pip install --upgrade pip
     
     cd ../..
     sudo rm -rf ./tempPy
-
 fi
 
 # Installing DeepNude
 
 cd ~/Documents # <----- Change this to change DeepNude local installation.
-
+    
 sudo apt install git
 pip3.9 install opencv_python numpy pillow setuptools six wheel torch torchvision
 
@@ -41,13 +38,11 @@ clear
 
 # Installing models
 
-clear
 echo "Do you want install models? [Y/n]"
 read insMod
 clear
 
 if [ $insMod == y ] || [ $insMod == Y ]; then 
-
     mkdir ./deepnude/checkpoints
     cd ./deepnude/checkpoints
     
@@ -55,7 +50,7 @@ if [ $insMod == y ] || [ $insMod == Y ]; then
     wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1OSEicdAAGrIBb7678RMGPmhuzdo8Yiql' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1OSEicdAAGrIBb7678RMGPmhuzdo8Yiql" -O mm.lib && rm -rf /tmp/cookies.txt
     wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1U98chaSdY9LH754t2jzPRmr_OowD2hRO' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1U98chaSdY9LH754t2jzPRmr_OowD2hRO" -O mn.lib && rm -rf /tmp/cookies.txt
     cd -
-
+    clear
 fi
 
 # Deactivating self.gpu_ids
@@ -66,9 +61,8 @@ clear
 
 if [ $disGpu == y ] || [ $disGpu == Y ]; then
     sed -i '64s/.*/		self.gpu_ids = [] #FIX CPU/' ./deepnude/gan.py
+    clear
 fi
-
-clear
 
 echo "code by adotDEV"
 echo "please visit https://github.com/adotdev/"
